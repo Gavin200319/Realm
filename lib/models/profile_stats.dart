3088ec872d9568ee6c19a99c63acc3c8ba32b07d
Gavin200319 <gavinnyabera18@gmail.com>
2026-07-12 22:12:31 +0300

    feat:add profile_stats.dart file

---
+++ lib/models/profile_stats.dart
@@ -0,0 +1,22 @@
+class ProfileStats {
+  final String userId;
+  final String username;
+  final int dropsCreated;
+  final int dropsUnlocked;
+
+  const ProfileStats({
+    required this.userId,
+    required this.username,
+    required this.dropsCreated,
+    required this.dropsUnlocked,
+  });
+
+  factory ProfileStats.fromMap(Map<String, dynamic> map) {
+    return ProfileStats(
+      userId: map['user_id'] as String,
+      username: map['username'] as String,
+      dropsCreated: (map['drops_created'] as num).toInt(),
+      dropsUnlocked: (map['drops_unlocked'] as num).toInt(),
+    );
+  }
+}
